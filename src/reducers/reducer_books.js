67da1e1775e7c88e61107a01bb27935efd49c7ab
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-10 20:48:53 +0000

    conditional rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: "JavScript: The Good Parts" },
-        { title: "You Don't Know JS: ES6 & Beyond" },
-        { title: "JavaScript Pocket Reference" },
-        { title: "Learning React: Functional Web Development with React and Flux" }        
+        { title: "JavScript: The Good Parts", pages: 101 },
+        { title: "You Don't Know JS: ES6 & Beyond", pages: 97 },
+        { title: "JavaScript Pocket Reference", pages: 118 },
+        { title: "Learning React: Functional Web Development with React and Flux", pages: 215 }        
     ]
 }
